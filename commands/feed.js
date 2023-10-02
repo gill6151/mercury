d491326e75b030e6ef0266f1d10b34d5db465161
hgw <hgw7@yahoo.com>
2023-10-02 02:11:25 +0000

    time zone support + some error handling

--- commands/feed.js
+++ commands/feed.js
@@ -6,6 +6,9 @@ let parser = new Parser({
     headers: {'User-Agent': config.feed.useragent},
 });
 const striptags = require("striptags");
+const moment = require('moment'); 
+const tz = require('moment-timezone');
+const timer = ms => new Promise(res => setTimeout(res, ms))
 
 async function sendUpstream(content) {
     var output = content.join("\n")
@@ -13,12 +16,31 @@ async function sendUpstream(content) {
     process.exit()
 }
 
+function errorMessage(error, code, extra) {
+    console.log(error.code)
+    if (code == "404") {
+        var error = "[04ERROR] 404: " + extra + " not found"
+    } else {
+        var error = "[04ERROR] Unknown error"
+    }
+    
+    parentPort.postMessage(error);
+    process.exit()
+}
+
 async function fetchFeed(feedURL, n) {
     var content = [];
-    let newFeed = await parser.parseURL(feedURL);
+    try {
+        var newFeed = await parser.parseURL(feedURL);
+    } catch (e) {
+        errorMessage(e, "404", feedURL);
+    }
     if (n > newFeed.items.length) {
         var n = newFeed.items.length;
         content.push("[08WARNING] Your requested post amount exceeded the total available. Reverting to " + newFeed.items.length);
+    } else if (n < 1) {
+        var n = 5
+        content.push("[08WARNING] You requested a number less than 1. Reverting to 5");
     }
     //for (let i = 0; i < newFeed.items.length; i++) {
     for (let i = 0; i < n; i++) {
@@ -29,12 +51,20 @@ async function fetchFeed(feedURL, n) {
         var body = data.contentSnippet.replace(/(\r\n|\n|\r)/gm, " ") //remove line breaks
             .replace(/\s{2,}/g, ' ') //idk
         var body = striptags(body);
+       if (data.isoDate !== undefined) {
+            var date = moment(data.isoDate)
+            var syncDate = date.tz(config.feed.timezone)
+            console.log(syncDate.format())
+            var date = syncDate.format(config.feed.time_format)
+        } else {
+            var date = data.pubDate
+        }
         if (body.length >= config.feed.body_max_chars) {
             var truncatedString = body.substring(0,config.feed.body_max_chars);
             var body = truncatedString + "..."
         }
         console.log(data);
-        var string = "15[11" + data.pubDate + "15] - 08" + title + " - " + body + " - " + data.link;
+        var string = "15[11" + date + "15] 08" + title + " " + body + " " + data.link;
         var output = string;
         content.push(output)
     }
--- commands/twitter.js
+++ commands/twitter.js
@@ -6,8 +6,9 @@ let parser = new Parser({
     headers: {'User-Agent': config.feed.useragent},
 });
 const striptags = require("striptags");
+const moment = require('moment'); 
+const tz = require('moment-timezone');
 const timer = ms => new Promise(res => setTimeout(res, ms))
-
 
 async function sendUpstream(content) {
     var output = content.join("\n")
@@ -15,19 +16,44 @@ async function sendUpstream(content) {
     process.exit()
 }
 
+function errorMessage(error, code, extra) {
+    console.log(error.code)
+    if (code == "404") {
+        var error = "[04ERROR] 404: " + extra + " not found"
+    } else {
+        var error = "[04ERROR] Unknown error"
+    }
+    
+    parentPort.postMessage(error);
+    process.exit()
+}
+
 async function fetchFeed(feedURL, n) {
     var content = [];
+
+    if (feedURL.startsWith('@') == true) {
+        var feedURL = feedURL.substring(1,feedURL.length);
+    }
     var randomNitter = config.twitter.nitter_instances[Math.floor(Math.random() * config.twitter.nitter_instances.length)];
     var feedURL = "https://" + randomNitter + "/" + feedURL + "/rss"
-    let newFeed = await parser.parseURL(feedURL);
-    console.log(newFeed.items.length);
+    try {
+        var newFeed = await parser.parseURL(feedURL);
+    } catch (e) {
+        errorMessage(e, "404", feedURL);
+    }
+    
     if (n > newFeed.items.length) {
         var n = newFeed.items.length;
         content.push("[08WARNING] Your requested post amount exceeded the total available. Reverting to " + newFeed.items.length);
+    } else if (n < 1) {
+        var n = 5
+        content.push("[08WARNING] You requested a number less than 1. Reverting to 5");
     }
-    //for (let i = 0; i < newFeed.items.length; i++) {
+
     for (let i = 0; i < n; i++) {
+
         await timer(50);
+
         var data = newFeed.items[i]
         var title = data.title.replace(/(\r\n|\n|\r)/gm, " ") //remove line breaks
             .replace(/\s{2,}/g, ' ') //idk
@@ -35,12 +61,22 @@ async function fetchFeed(feedURL, n) {
         var body = data.contentSnippet.replace(/(\r\n|\n|\r)/gm, " ") //remove line breaks
             .replace(/\s{2,}/g, ' ') //idk
         var body = striptags(body);
+
+        if (data.isoDate !== undefined) {
+            var date = moment(data.isoDate)
+            var syncDate = date.tz(config.feed.timezone)
+            console.log(syncDate.format())
+            var date = syncDate.format(config.feed.time_format)
+        } else {
+            var date = data.pubDate
+        }
+
         if (body.length >= config.feed.body_max_chars) {
             var truncatedString = body.substring(0,config.feed.body_max_chars);
             var body = truncatedString + "..."
         }
-        console.log(data);
-        var string = "15[11" + data.pubDate + "15] 08" + data.creator + " " + body + " " + data.link;
+        //console.log(data);
+        var string = "15[11" + date + "15] 08" + data.creator + " " + body + " " + data.link;
         var output = string;
         content.push(output)
     }
